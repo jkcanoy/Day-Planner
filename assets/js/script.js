c4deee030c11e947af2b47ea8ed3fbd02873bed2
Kyle Canoy <kcanoy@uw.edu>
2021-04-28 15:25:09 -0700

    added row for hour, reminder, and save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,16 @@ getCurrentDate();
 setInterval(getCurrentDate, 1000);
 
 // for loop to create rows for timeblocks
-for (i = 0;i < 9; i++);
+for (i = 0;i < 9; i++) {
+    var rowEl = $("<div>").addClass("row")
+    var hourRowEl = $("<div>").addClass("hour col-md-2").text(moment("9:00 AM", "hh:mm A").add(i, "hours").format('hA'));
+        hourRowEl.attr("date-time", moment("9:00 AM", "hh:mm A").add(i, "hours").format('hA'));
+    var reminderInputEl = $("<textarea>").addClass("col-md-9");
+    var saveBtnEl = $("<button>").addClass("saveBtn col-md-1");
+}
 
+// append rows to container
+$(".container").append(rowEl);
+$(rowEl).append(hourRowEl);
+$(hourRowEl).after(reminderInputEl);
+$(reminderInputEl).after(saveBtnEl);
